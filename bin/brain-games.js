7ff9db1eea8e48c9abe1fedabe26465164c433e2
siemchic <siemchic@mail.ru>
2024-03-02 09:31:23 +0500

    js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import WhatName from '../src/cli';
+import WhatName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 // const name = readlineSync.question('May I have your name? ');
