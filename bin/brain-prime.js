52a846d6b1931f56a94010dc958b3b5b4f17efe6
siemchic <siemchic@mail.ru>
2024-03-17 23:47:45 +0500

    brain-prime_V2

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -6,19 +6,44 @@ import {
 // eslint-disable-next-line import/extensions
 } from '../src/index.js';
 
-function contains(mass, vopros) {
+/* function contains(mass, vopros) {
   for (let j = 0; j < mass.length; j += 1) {
     if (mass[j] === vopros) {
       return true;
     }
   }
   return false;
+} */
+function prost(x) {
+  const mass = [];
+  for (let i = 1; i <= x; i += 1) {
+    const delitel = x % i;
+    if (delitel === 0) {
+      mass.push(delitel);
+    }
+  }
+  if (mass.length === 2) {
+    return 'yes';
+  }
+  return 'no';
 }
 
 const name = WhatName();
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+for (let i = 0; i < 3; i += 1) {
+  const vopros = getRandomNumber(1, 100);
+  console.log(`Question: ${vopros}`);
+  const answer = readlineSync.question('Your answer: ');
 
-const mass = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+  if (answer === prost(vopros)) {
+    console.log('Correct');
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${prost(vopros)}'.`);
+    console.log(`Let's try again, ${name}!`);
+    break;
+  } Congratulations(name, i);
+}
+/* const mass = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
   41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 // цикл с вопросами
 for (let i = 0; i < 3; i += 1) {
@@ -40,6 +65,5 @@ for (let i = 0; i < 3; i += 1) {
     console.log(`Let's try again, ${name}!`);
     break;
   }
-
   Congratulations(name, i);
-}
+} */
